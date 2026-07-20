f337b4f8ca18bd538f226ba0541e47e6806d51de
sakshamsharma31789-svg <sakshamsharma31789@gmail.com>
2026-07-20 14:08:49 +0530

    Step 14: output_spatial_size

--- model.py
+++ model.py
@@ -113,8 +113,12 @@ def pad_2d(images, pad):
 
     pass
 
-# Step 14 - output_spatial_size (not yet solved)
-# TODO: implement
+# Step 14 - output_spatial_size
+def output_spatial_size(input_size, kernel, stride, padding):
+    # TODO: return the conv/pool output spatial dimension from input_size, kernel, stride, padding
+    return int((input_size + 2*padding - kernel)/stride + 1)
+    
+    pass
 
 # Step 15 - im2col (not yet solved)
 # TODO: implement
