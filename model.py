d27add89ca11af6c4fea452b426593cc60bfb13d
sakshamsharma31789-svg <sakshamsharma31789@gmail.com>
2026-07-20 11:44:11 +0530

    Step 12: init_zero_bias

--- model.py
+++ model.py
@@ -97,8 +97,13 @@ def he_init(shape, fan_in, seed):
     return np.random.normal(loc=0.0,scale=std,size=shape)
     pass
 
-# Step 12 - init_zero_bias (not yet solved)
-# TODO: implement
+# Step 12 - init_zero_bias
+import numpy as np
+
+def init_zero_bias(length):
+    # TODO: return a 1D float array of zeros with the given length.
+    return (np.zeros(length,dtype=np.float64))
+    pass
 
 # Step 13 - pad_2d (not yet solved)
 # TODO: implement
